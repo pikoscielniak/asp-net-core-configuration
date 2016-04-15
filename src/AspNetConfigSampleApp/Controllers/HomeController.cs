39f8994373c4c8510d5ec32dc072d98f53edeb3c
Piotr Kościelniak <piotrek.koscielniak@gmail.com>
2016-04-16 01:03:31 +0200

    injectino options in view

--- src/AspNetConfigSampleApp/Controllers/HomeController.cs
+++ src/AspNetConfigSampleApp/Controllers/HomeController.cs
@@ -5,16 +5,9 @@ namespace AspNetConfigSampleApp.Controllers
 {
     public class HomeController : Controller
     {
-        private ClientSettings _clientSettings;
-
-        public HomeController(IOptions<ClientSettings> clientSettings)
-        {
-            _clientSettings = clientSettings.Value;
-        }
         public IActionResult Index()
-        {            
-              var clientName = _clientSettings;
-            return Content($"{_clientSettings.Name} {_clientSettings.PageSize} {_clientSettings.ShowTitle}");
+        {
+            return View();
         }
     }
 }
--- src/AspNetConfigSampleApp/Startup.cs
+++ src/AspNetConfigSampleApp/Startup.cs
@@ -30,7 +30,8 @@ namespace AspNetConfigSampleApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
+            app.UseDeveloperExceptionPage();
+            app.UseIISPlatformHandler();            
             
             app.UseMvcWithDefaultRoute();
         }
